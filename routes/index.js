54fa664f858b8e29115ab591f8216e4920bf6682
mosmn <ithinkisee8@gmail.com>
2023-09-23 08:54:34 +0800

    added the routing for creating a new message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/new", function (req, res, next) {
+  const message = req.body.message;
+  const user = req.body.user;
+  messages.push({ text: message, user: user, added: new Date() });
+  res.redirect("/");
+});
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages: messages });
 });
 
 module.exports = router;
